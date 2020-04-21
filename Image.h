f2aa913e1f98df401bb50aa51fc5257ba0dc7856
Joey Rieg <jurieg@htwg-konstanz.de>
2020-04-21 10:46:52 +0200

    [COLORTRANSFER] implement first working example, refactoring needed.

--- Image.h
+++ Image.h
@@ -12,7 +12,15 @@ class Image
 public:
     Image(int width, int height, int numChannels) : _width(width), _height(height), _inChannels(numChannels)
     {
-        _imageData.reserve(width*height*numChannels);
+        _imgSize = width * height * numChannels;
+        _imageData.resize(_imgSize);
+    }
+
+    Image(const std::vector<unsigned char>& imgData, int width, int height, int numChannels) :
+            _width(width), _height(height), _inChannels(numChannels)
+    {
+        _imgSize = width * height * numChannels;
+        _imageData = imgData;
     }
 
     Image(const std::string& path, int numComponents = 0)
@@ -24,12 +32,59 @@ public:
                 numComponents == 0 ? 0 : STBI_COMPONENTS[numComponents-1]);
         if(!imgData) { std::cerr << "IMAGE ERROR: could not load image from path:" << path << "\n"; }
 
-        _imageData.assign(imgData, imgData + _width*_height*_inChannels);
-
+        _imgSize = _width * _height * _inChannels;
+        _imageData.assign(imgData, imgData + _imgSize);
         delete imgData;
     }
 
-    void pixelColor(const int x, const int y, int& r, int& g, int& b) const
+    void calcAxisMeans(double& xMean, double& yMean, double& zMean) const
+    {
+        double a =0.0, b = 0.0, c=0.0;
+        for (int i = 0; i < _imgSize; i++)
+        {
+            if(_imageData[i] != _imageData[i] || _imageData[i+1] != _imageData[i+1] || _imageData[i+2] != _imageData[i+2]){
+                int a = _imageData[i];
+                int b = _imageData[i+1];
+                int c = _imageData[i+2];
+            }
+           a += (_imageData[i]);
+           b += (_imageData[++i]);
+           c += (_imageData[++i]);
+
+        }
+
+        double pixelsPerChannel = static_cast<double>(_imgSize)/3.0;
+        xMean /= pixelsPerChannel;
+        yMean /= pixelsPerChannel;
+        zMean /= pixelsPerChannel;
+    }
+
+    void calcStandardDeviation(double xMean, double yMean, double zMean, double& xStd, double& yStd, double& zStd) const
+    {
+        //sqrt(1/(numPixels-1) * sum(x_i - x)^2)
+        double xSum = 0, ySum = 0, zSum = 0;
+        for (int i = 0; i < _imgSize; i++)
+        {
+            double x = static_cast<double>(_imageData[i]);
+            double y = static_cast<double>(_imageData[++i]);
+            double z = static_cast<double>(_imageData[++i]);
+            xSum += pow(x - xMean,2);
+            ySum += pow(y - yMean, 2);
+            zSum += pow(z - zMean, 2);
+        }
+
+        int numPixels = _width*height();
+        xStd = sqrt(1.0/(numPixels-1) * xSum);
+        yStd = sqrt(1.0/(numPixels-1) * ySum);
+        zStd = sqrt(1.0/(numPixels-1) * zSum);
+    }
+
+    void pixelData(std::vector<unsigned char>& pData) const
+    {
+        pData = _imageData;
+    }
+
+    void pixelColor(const int x, const int y, unsigned char& r, unsigned char& g, unsigned char& b) const
     {
         int idx = y*_width*_inChannels + x *_inChannels;
         r = _imageData[idx];
@@ -37,17 +92,48 @@ public:
         b = _imageData[++idx];
     }
 
+    void pixelColor(int idx, unsigned char& r, unsigned char& g, unsigned char& b) const
+    {
+        r = _imageData[idx];
+        g = _imageData[++idx];
+        b = _imageData[++idx];
+    }
+
+    void setPixel(const int x, const int y, unsigned char r, unsigned char g , unsigned char b) {
+        if (x > _width || y >= _height || x < 0 || y < 0) {
+            std::cerr << "IMAGE ERROR: wrong dimension for x or y when setting pixel color!\n";
+            return;
+        }
+        int idx = y*_width*_inChannels + x *_inChannels;
+        setPixel(idx, r, g, b);
+    }
+
+    void setPixel(int idx, const unsigned char r, const unsigned char g, const unsigned char b)
+    {
+        if (idx > _imgSize) {
+            std::cerr << "IMAGE ERROR: wrong index when setting pixel color! Max index is " << _imgSize << "\n";
+            return;
+        }
+
+        _imageData[idx] = r;
+        _imageData[++idx] = g;
+        _imageData[++idx] = b;
+    }
+
     void write(const std::string& path, int outChannels= 0)
     {
-        stbi_write_jpg(path.c_str(), _width, _height, outChannels == 0 ? _inChannels : outChannels, &_imageData[0], 100);
+        // TODO: correct rounding on converting to unsigned char vector
+        stbi_write_jpg(path.c_str(), _width, _height, outChannels == 0 ? _inChannels : outChannels,
+                    &_imageData[0], 100);
     }
 
     int width() const { return _width; }
     int height() const { return _height; }
     int channels() const { return _inChannels; }
+    long size() const { return _imgSize; }
+    std::vector<unsigned char> getData() { return _imageData; }
 
 private:
-    int _width, _height, _inChannels;
-    //unsigned char* _imageData;
+    int _width, _height, _inChannels, _imgSize;
     std::vector<unsigned char> _imageData;
 };
--- main.cpp
+++ main.cpp
@@ -1,143 +1,161 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
-#include <xtensor/xarray.hpp>
-#include <xtensor/xmath.hpp>
-#include <xtensor/xio.hpp>
-#include <xtensor/xadapt.hpp>
-#include <xtensor/xmanipulation.hpp>
-#include <xtensor/xbroadcast.hpp>
-#include <xtensor-blas/xlinalg.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/matrix_operation.hpp>
 
 #include "Image.h"
 
-// Returns the transformation matrix from RGB to LMS space.
-// Done in seperate steps for educational purposes.
-xt::xarray<double> getRGB2LMS()
+void calcLAlphaBetaImg(const Image& img, std::vector<float>& lAlphaBetaImg)
 {
-    // International Telecomunication Union standard matrix
-    xt::xarray<double> M_itu{{0.4306, 0.3415, 0.1784},
-                             {0.2220, 0.7067, 0.0713},
-                             {0.0202, 0.1295, 0.9394}};
+    glm::mat3 M_rgb2lms = glm::transpose(glm::mat3(0.3811, 0.5783, 0.0402,
+                                                   0.1967, 0.7244, 0.0782,
+                                                   0.0241 ,0.1288, 0.8444));
 
-    // First map RGB->XYZ. Map RGB white to XYZ white. White in XYZ chromacity diagram is given by
-    // x = X / (X*Y*Z) = 0.33 and
-    // y = y / (X*Y*Z) = 0.33
-    // --> Modify the XYZ_itu standard conversion matrix to have rows that add up to 1
-    xt::xarray<double> V_x{1.0, 1.0, 1.0};
+    glm::vec3 diag(1.0f/glm::sqrt(3),1.0f/glm::sqrt(6), 1.0f/glm::sqrt(2));
+    glm::mat3 M_lms2lab = glm::transpose(glm::diagonal3x3(diag)) * glm::transpose(glm::mat3(1, 1, 1,
+                                                             1, 1, -2,
+                                                             1, -1, 0));
 
-    // Solve M_itu * V_x = (1 1 1)^T for x
-    xt::xarray<double> V_xItuTransform = xt::linalg::dot(xt::linalg::inv(M_itu),xt::transpose(V_x));
+    for (int idx = 0; idx < img.size(); idx+=3)
+    {
+        unsigned char r, g, b;
+        img.pixelColor(idx, r, g, b);
+        glm::vec3 rgbColor(static_cast<float>(r)/255.0f, static_cast<float>(g)/255.0f, static_cast<float>(b)/255.0f);
 
+        glm::vec3 lmsColor = M_rgb2lms * rgbColor;
+        lmsColor = glm::vec3(glm::max(FLT_MIN, lmsColor.r),
+                             glm::max(FLT_MIN, lmsColor.g),
+                             glm::max(FLT_MIN, lmsColor.b));
 
-    std::cout <<"Standard conversion matrix:\n" << M_itu << "\n\n";
+        // log10(x) = ln(x) / ln(10)
+        glm::vec3 lmsLogColor = glm::log(lmsColor) / glm::log(10.0f);
 
-    // Multiply columns of M_itu to get final conversion matrix
-    xt::xarray<double> M_rgbToXyz = M_itu * V_xItuTransform;
+        glm::vec3 lALphaBetaColor = M_lms2lab * lmsLogColor;
 
-    std::cout << "RGB2XYZ:\n" << M_rgbToXyz << "\n\n";
+        lAlphaBetaImg.push_back(lALphaBetaColor.r);
+        lAlphaBetaImg.push_back(lALphaBetaColor.g);
+        lAlphaBetaImg.push_back(lALphaBetaColor.b);
 
-    // [X Y Z]^T = M_ituTransform * [R G B]^T
-    // XYZ values can be further converted to LMS space with another transformation matrix
-    xt::xarray<double> M_xyzToLms = {{0.3897, 0.6890, -0.0787},
-                                     {-0.2298, 1.1834, 0.0464},
-                                     {0.0000, 0.0000, 1.0000}};
-
-    // Combining the RGB to XYZ and XYZ to LMS matrices results in a transformation matrix
-    // that maps from RGB to LMS space
-    // The resulting matrix doesn't comply with the paper matrix but the rows actually sum up to exactly 1
-    xt::xarray<double> M_rgbToLms = xt::linalg::dot(M_xyzToLms,M_rgbToXyz);
-
-    std::cout <<"RGB2LMS:\n" << M_rgbToLms << "\n\n";
-
-    return M_rgbToLms;
+    }
 }
 
-xt::xarray<double> getLMS2LAlphaBeta()
+void calcAxisMeans(const std::vector<float>& imgData, glm::vec3& means)
 {
-    xt::xarray<double> LMS2LAlphaBeta = xt::linalg::dot(xt::sqrt(xt::xarray<double>{{1.0/3.0, 0 ,0 },
-                                                                     {0, 1.0/6.0, 0},
-                                                                     {0, 0, 1.0/2.0}}),
-                                         xt::xarray<double>{{1.0, 1.0, 1.0},
-                                                            {1.0, 1.0, -2.0},
-                                                            {1.0, -1.0, 0.0}});
-    return LMS2LAlphaBeta;
+    const int numValues = imgData.size();
+    glm::vec3 sum(0);
+    for(int idx = 0; idx < numValues; idx++)
+    {
+        sum.x += imgData[idx];
+        sum.y += imgData[++idx];
+        sum.z += imgData[++idx];
+    }
+    means = sum / static_cast<float>(numValues);
 }
 
-xt::xarray<double> getLAlphaBeta2LMS()
+void calcAxisStds(const std::vector<float>& imgData, const glm::vec3& means, glm::vec3& stds)
 {
-    // Weird as well, because the diagonal matrix isn't the inverse of the diagonal matrix in the "forward pass"
-    xt::xarray<double> LAlphaBeta2LMS = xt::linalg::dot(xt::xarray<double>{{1.0, 1.0, 1.0},
-                                                                           {1.0, 1.0, -1.0},
-                                                                           {1.0, -2.0, 0.0}},
-                                                        xt::sqrt(xt::xarray<double>{{1.0/3.0, 0 ,0 },
-                                                                                    {0, 1.0/6.0, 0},
-                                                                                    {0, 0, 1.0/2.0}}));
-    return LAlphaBeta2LMS;
+    const int numValues = imgData.size();
+    glm::vec3 sum(0);
+    for (int idx = 0; idx < numValues; idx++)
+    {
+        sum.x += glm::pow(imgData[idx] - means.x,2);
+        sum.y += glm::pow(imgData[++idx] - means.y,2);
+        sum.z += glm::pow(imgData[++idx] - means.z,2);
+    }
+
+    stds = glm::sqrt(1.0f/ (static_cast<float>(numValues)-1.0f) * sum);
+}
+
+void transfer(std::vector<float>& lAlphaBetaData, const glm::vec3& srcMeans, const glm::vec3& srcStds,
+                const glm::vec3& transMeans, const glm::vec3& transStds)
+{
+    glm::vec3 lALphaBetaColor;
+    for (int idx = 0; idx < lAlphaBetaData.size(); idx+=3)
+    {
+        lAlphaBetaData[idx] = (transStds.x/srcStds.x) * (lAlphaBetaData[idx] - srcMeans.x) + transMeans.x;
+        lAlphaBetaData[idx+1] = (transStds.y/srcStds.y) * (lAlphaBetaData[idx+1] - srcMeans.y) + transMeans.y;
+        lAlphaBetaData[idx+2] = (transStds.z/srcStds.z) * (lAlphaBetaData[idx+2] - srcMeans.z) + transMeans.z;
+    }
+}
+
+void calcRGB(const std::vector<float>& lAlphaBetaData, Image& destImg)
+{
+    glm::mat3 M_lab2lms = glm::transpose(glm::mat3(1,1,1,
+                                    1,1, -1,
+                                    1, -2,0)) * glm::transpose(glm::diagonal3x3(glm::vec3(1/glm::sqrt(3),
+                                                                                        1/glm::sqrt(6),
+                                                                                        1/glm::sqrt(2))));
+    glm::mat3 M_lms2rgb = glm::transpose(glm::mat3(4.4679, -3.5873, 0.1193,
+                                                       -1.2186, 2.3809, -0.1624,
+                                                       0.0497, -0.2439, 1.2045));
+
+    for (int idx = 0; idx < lAlphaBetaData.size(); idx+=3)
+    {
+        glm::vec3 lAlphaBetaColor(lAlphaBetaData[idx], lAlphaBetaData[idx+1], lAlphaBetaData[idx+2]);
+        glm::vec3 lmsLogColor = M_lab2lms * lAlphaBetaColor;
+        glm::vec3 lmsColor = glm::pow(glm::vec3(10), lmsLogColor);
+        glm::vec3 rgbColor = M_lms2rgb * lmsColor;
+        rgbColor = glm::clamp(rgbColor, 0.0f, 1.0f);
+        destImg.setPixel(idx, static_cast<unsigned char>(255.0f * rgbColor.r),
+                              static_cast<unsigned char>(255.0f * rgbColor.g),
+                              static_cast<unsigned char>(255.0f * rgbColor.b));
+    }
 }
 
 void transferColor(const Image& sourceImg, const Image& transferImg, Image& destinationImg)
 {
-    // Initialization of the matrices. Further explanation in the corresponding methods.
-    // The transformation back from L-alpha-beta space to RGB are the inverse transformations.
-    xt::xarray<double> M_Rgb2Lms = getRGB2LMS();
-    std::cout << "RBG2LMS\n" << M_Rgb2Lms<< "\n\n";
-    xt::xarray<double> M_Lms2LAlphaBeta = getLMS2LAlphaBeta();
-    std::cout << "LMS2LAlphaBeta:\n" << M_Lms2LAlphaBeta<< "\n\n";
-    xt::xarray<double> M_LAlphaBeta2LMS = getLAlphaBeta2LMS();
-    std::cout << "LAlphaBeta2Lms:\n" << M_LAlphaBeta2LMS<< "\n\n";
-    xt::xarray<double> M_Lms2Rgb = xt::linalg::inv(M_Rgb2Lms);
-    std::cout << "LMS2Rgb:\n" << M_Lms2Rgb<< "\n\n";
+    std::vector<float> srcLAlphaBeta, transLAlphaBeta;
+    srcLAlphaBeta.reserve(sourceImg.size());
+    transLAlphaBeta.reserve(transferImg.size());
+    calcLAlphaBetaImg(sourceImg, srcLAlphaBeta);
+    calcLAlphaBetaImg(transferImg, transLAlphaBeta);
 
-    //M_rgbToLms[0] = xt::log10<double>(M_rgbToLms[0]);
+    const auto [minS, maxS] = std::minmax_element(srcLAlphaBeta.begin(), srcLAlphaBeta.end());
+    const auto [minT, maxT] = std::minmax_element(transLAlphaBeta.begin(), transLAlphaBeta.end());
+    std::cout << "min src: " << *minS << " max src: " << *maxS << std::endl;
+    std::cout << "min trans: " << *minT << " max trans: " << *maxT << std::endl;
 
-    for (int i = 0; i < sourceImg.width(); i++) {
-        for (int j = 0; j < sourceImg.height(); j++) {
-            int r,g,b;
-            sourceImg.pixelColor(i,j,r,g,b);
-            xt::xarray<double> rgbColor{r,g,b};
-            std::cout << "Initial RGB:\n" << rgbColor << "\n\n";
+    glm::vec3 srcMeans, srcStds, transMeans, transStds;
+    calcAxisMeans(srcLAlphaBeta, srcMeans);
+    calcAxisMeans(transLAlphaBeta, transMeans);
+    std::cout << "SRC means " << srcMeans.x << " " << srcMeans.y << " " << srcMeans.z << std::endl;
+    std::cout << "TRANS means " << transMeans.x << " " << transMeans.y << " " << transMeans.z << std::endl;
 
-            xt::xarray<double> lmsColor = xt::linalg::dot(M_Rgb2Lms, rgbColor);
-            std::cout << "LMS:\n" << lmsColor << "\n\n";
+    calcAxisStds(srcLAlphaBeta, srcMeans, srcStds);
+    calcAxisStds(transLAlphaBeta, transMeans, transStds);
+    std::cout << "SRC stds " << srcStds.x << " " << srcStds.y << " " << srcStds.z << std::endl;
+    std::cout << "TRANS stds " << transStds.x << " " << transStds.y << " " << transStds.z << std::endl;
 
-            // "The data in [the LMS] color space shows a great deal of skew, which we can largely eliminate by
-            // converting the data to logarithmic space"
-            lmsColor = xt::log10(lmsColor);
-            std::cout << "LMS-log:\n" << lmsColor << "\n\n";
+    transfer(srcLAlphaBeta, srcMeans, srcStds, transMeans, transStds);
 
-            xt::xarray<double> lAlphaBetaColor = xt::linalg::dot(M_Lms2LAlphaBeta, lmsColor);
-            std::cout << "LAlphaBeta:\n" << lAlphaBetaColor << "\n\n";
-
-            // TODO: color correction with stds and means
-
-            // Converting back to RGB
-            lmsColor = xt::linalg::dot(M_LAlphaBeta2LMS, lAlphaBetaColor);
-            std::cout << "LMS-log:\n" << lmsColor << "\n\n";
-
-            lmsColor = xt::pow(10,lmsColor);
-            std::cout << "LMS:\n" << lmsColor << "\n\n";
-
-            rgbColor = xt::linalg::dot(M_Lms2Rgb, lmsColor);
-
-            std::cout << "Final RGB:\n" << rgbColor << "\n\n";
-
-
-        }
-    }
+    calcRGB(srcLAlphaBeta, destinationImg);
 }
-
 
 int main()
 {
-    Image srcImg("../Images/ocean1.jpg");
-    Image transferImg("../Images/sunset1.jpg");
+    glm::vec3 a(-0.1, 0.5, 1.0004);
+    glm::clamp(a, 0.0f, 1.0f);
+    Image srcImg("../Images/mountain_lake.jpg");
+    Image transferImg("../Images/ocean1.jpg");
     Image dstImg(800, 600, 3);
 
     transferColor(srcImg, transferImg, dstImg);
 
-    transferImg.write("../Images/ocean_processed.jpg");
+    /*std::vector<unsigned char> srcData;
+    srcImg.pixelData(srcData);
+    for (int idx = 0; idx < srcData.size(); idx+=3)
+    {
+        glm::vec3 lAlphaBetaColor(srcData[idx], srcData[idx+1], srcData[idx+2]);
+        lAlphaBetaColor /= 255.0f;
+        dstImg.setPixel(idx, static_cast<unsigned char>(255.0f*lAlphaBetaColor.r),
+                         static_cast<unsigned char>(255.0f*lAlphaBetaColor.g),
+                         static_cast<unsigned char>(255.0f*lAlphaBetaColor.b));
+    }*/
+
+    dstImg.write("../Images/ocean_processed.jpg");
 
     return 0;
 }
